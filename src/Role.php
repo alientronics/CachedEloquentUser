c0c302c68df93217fdea09b4119a912ab830b9c4
rafaelpv <rafaelpviegas@gmail.com>
2016-04-27 21:48:42 -0300

    Added files via upload

---
+++ src/Role.php
@@ -0,0 +1,23 @@
+<?php 
+
+namespace Alientronics\CachedEloquentUser;
+
+use Kodeine\Acl\Models\Eloquent\Role as KodeineRole;
+use Illuminate\Support\Facades\Auth;
+
+class Role extends KodeineRole {
+
+    /**
+     * List all permissions
+     *
+     * @return mixed
+     */
+    public function getPermissions()
+    {
+        return \Cache::remember('permissionById_'.Auth::user()['id'], 60,
+            function () {
+                return $this->getPermissionsInherited();
+            }
+        );
+    }
+}
